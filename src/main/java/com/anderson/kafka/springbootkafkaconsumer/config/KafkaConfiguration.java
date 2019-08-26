f170619d534630fe257a5faf2c964021fcd342d7
anderson yang <b4657892002@gmail.com>
2019-08-26 16:40:10 +0800

    Initial commit

---
+++ src/main/java/com/anderson/kafka/springbootkafkaconsumer/config/KafkaConfiguration.java
@@ -0,0 +1,105 @@
+package com.anderson.kafka.springbootkafkaconsumer.config;
+
+import com.anderson.kafka.springbootkafkaconsumer.model.User;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConfiguration
+{
+
+
+    //String
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory()
+    {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "140.118.109.106:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory()
+    {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+//        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory();
+//        factory.setConsumerFactory(consumerFactory());
+//        return factory;
+    }
+
+
+    //Json
+    @Bean
+    public ConsumerFactory<String, User> userConsumerFactory()
+    {
+        Map<String, Object> config = new HashMap<>();
+
+//        https://stackoverflow.com/questions/51688924/spring-kafka-the-class-is-not-in-the-trusted-packages
+        JsonDeserializer<User> deserializer = new JsonDeserializer<>(User.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeMapperForKey(true);
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "140.118.109.106:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), deserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory userKafkaListenerFactory()
+    {
+        ConcurrentKafkaListenerContainerFactory<String, User> factory = new ConcurrentKafkaListenerContainerFactory();
+        factory.setConsumerFactory(userConsumerFactory());
+        return factory;
+    }
+
+
+    //File
+    @Bean
+    public ConsumerFactory<Integer, byte[]> fileConsumerFactory()
+    {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "140.118.109.106:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<Integer, byte[]> fileKafkaListenerContainerFactory()
+    {
+        ConcurrentKafkaListenerContainerFactory<Integer, byte[]> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(fileConsumerFactory());
+        return factory;
+//        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory();
+//        factory.setConsumerFactory(consumerFactory());
+//        return factory;
+    }
+
+}
---
+++ src/main/java/com/anderson/kafka/springbootkafkaconsumer/listener/KafkaConsumer.java
@@ -0,0 +1,79 @@
+package com.anderson.kafka.springbootkafkaconsumer.listener;
+
+import com.anderson.kafka.springbootkafkaconsumer.model.User;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+@Service
+public class KafkaConsumer
+{
+    private final byte[] finalBytes = {0x00, 0x11};
+    FileOutputStream fileOutputStream;
+
+    {
+        try
+        {
+            fileOutputStream = new FileOutputStream("D:\\testFile\\output\\test_550MB.exe");
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @KafkaListener(topics = "Kafka_Example", groupId = "group_id")
+    public void consume(String message)
+    {
+        System.out.println("Consumed message: " + message);
+    }
+
+    @KafkaListener(topics = "Kafka_Example_json", groupId = "group_json", containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user)
+    {
+        System.out.println("Consumed JSON Message: " + user);
+    }
+
+    @KafkaListener(topics = "Kafka_Example_file", groupId = "group_file", containerFactory = "fileKafkaListenerContainerFactory")
+    public void consumeFile(ConsumerRecord<Integer, byte[]> record)
+    {
+        System.out.println("Consumed file Message: key " + record.key() + ", value " + Arrays.toString(record.value()));
+        byte[] receivedBytes = record.value();
+        if(Arrays.equals(record.value(), finalBytes))
+        {
+            System.out.println("Consume over");
+            try
+            {
+                fileOutputStream.close();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return;
+        }
+        try {
+            fileOutputStream.write(receivedBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private static final String TOPIC = "Kafka_Example_json";
+
+    //TestFileTransfer
+    private byte[] concatenateByteArray(byte[] a, byte[] b)
+    {
+
+
+        byte[] c = new byte[a.length + b.length];
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+        return c;
+    }
+}
